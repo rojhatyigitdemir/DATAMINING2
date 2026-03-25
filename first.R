21e679870f2aa7faa9938a90b28f9fdd9f11d672
rojhatyigitdemir <rojhatyigitdemir@gmail.com>
2026-03-25 15:04:21 +0100

    BTC spot prices and handling NA values

--- first.R
+++ first.R
@@ -1,1 +1,22 @@
-hello
+# Script 1: real bitcoin prices (Dependent Variable)
+library(quantmod)
+
+# BTC-USD from 2016 until today (daily)
+getSymbols("BTC-USD", src = "yahoo", from = "2026-01-01", periodicity = "daily")
+
+# RAW DATA TO DATAFRAME
+btc_daily <- data.frame(Date = index(`BTC-USD`), coredata(`BTC-USD`))
+
+# I NEED JUST CLOSING PRICES
+btc_clean <- btc_daily[, c("Date", "BTC.USD.Close")]
+colnames(btc_clean) <- c("Date", "BTC_Spot_Price")
+
+btc_clean <- na.omit(btc_clean)
+
+# Let's check the DATA
+
+print(head(btc_clean))
+
+# SAVE DATA TO GITIGNORE instead of directly to hitgub
+if(!dir.exists("01-data_raw")) dir.create("01-data_raw")
+write.csv(btc_clean, "01-data_raw/btc_spot_daily.csv", row.names = FALSE)
